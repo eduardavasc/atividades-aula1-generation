2ba37c4d51d30d8d5e1587e26ea9750b441f80d7
Maria Eduarda Vasconcelos <eduuardavasconcelos@gmail.com>
2023-12-07 12:52:07 -0300

    adicionando atividades pedidas em sala

---
+++ atividade01.java
@@ -0,0 +1,26 @@
+package atividadesaula01;
+
+import java.util.Scanner;
+
+public class atividade01 {
+
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+		
+		float salario, abono, novoSalario;
+		
+		System.out.println("Digite o valor do salário:" );
+		salario = leia.nextFloat();	
+		System.out.println("Digite o valor do abono:");
+		abono = leia.nextFloat();	
+		
+		novoSalario = salario + abono;
+		
+		System.out.printf("O novo salário é: %.2f", novoSalario);
+		
+			
+
+	}
+
+}
